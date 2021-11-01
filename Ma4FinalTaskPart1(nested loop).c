5b8e8d387ea1539228d70af882a7ac5291efccc2
Juvette01 <89185641+Juvette01@users.noreply.github.com>
2021-11-01 10:48:59 +0800

    Add files via upload

---
+++ Ma4FinalTaskPart1(nested loop).c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int x;
+    int y;
+    for(x=1; x<=9; x++){
+    for(y=x; y<=9; y++){
+    printf("%d",y);
+    
+    }
+    
+    printf("\n");
+    
+    }
+   
+    
+    
+    
+    
+    return 0;
+}
